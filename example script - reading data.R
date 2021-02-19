8c33051993317c09b1ae1e5a7ea2739350032b37
annahubb <79106910+annahubb@users.noreply.github.com>
2021-02-19 10:10:55 -0800

    Add files via upload

---
+++ example script - reading data.R
@@ -0,0 +1,168 @@
+################################################################
+##### INSTRUCTIONS TO RUN PROGRAM HERE
+################################################################
+    # - This program pools multiple imputation results from descriptive analyses, bivariate correlation analyses, and single-level linear regression analyses.
+    # 
+    # - This program is only able to read in multiply imputed data from .csv , .dat , .Rdata , .txt tab delimited files where the multiply imputed dataset is in a stacked format where the unimputed data is at the top of the dataset.
+    # 
+    # - This program also requires the pooling.function.R and the data file to be in the same folder/file location as this program
+    # 
+    # - A 'mids' object (multiply-imputed-data-set) object is created when reading in the stacked data, that is used throughout the file. A user of the MICE package may already have the required object, named 'implist.mids' created
+
+################################################################
+##### USER INPUTS HERE 
+################################################################
+# data file name could be .dat, .csv, .Rdata, .txt
+# data file must be in a stacked file with un-imputed data at the top
+# input data cannot contain column headers/variable names
+    data  <- "reading.imps.dat"
+    
+# write the variable names here in the order they appear in the original file
+    # must include names for ALL columns in data file 
+    var.names <- c("imp","read1", "lrnprob1", "read2")
+    
+# write the indicator for the imputation number; must match variable name from 'var.names'
+    imp.id <- "imp"
+    
+# write the indicator for the case/participant number here; must match variable name from 'var.names'
+    # if there is not indicator variable for case/participant, use NA
+    participant.id <- NA
+    
+# write the indicator for missing values here
+    missing <- 999
+    
+# if computing/pooling descriptive statistics, write variables of interest here; must match variable names from 'var.names'
+    variables <- c("read1", "lrnprob1", "read2")   
+
+# if computing/pooling regression results, 
+    # write linear model here in the format 'outcome ~ predictor1 + predictor2 + ... + predictorK'; 
+    # must match variable names from 'var.names'
+    my.lm <- "read2 ~ read1 lrnprob1 read1*lrnprob1"
+
+################################################################
+##### SET WORKING DIRECTORY, LOAD FUNCTIONS, REQUIRED PACKAGES 
+################################################################
+# R studio function that defines the working directory as where the current file is saved
+# No modification required here
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+    
+# R function that defines the working directory to specific location; modify as needed
+# setwd("insert working directory file path here")
+    
+# check working directory here    
+getwd()
+
+# install required packages here; un-comment next line if needed
+# install.packages(c("dplyr", "mice", "psych", "rlist", "miceadds"))
+
+# load functions and required packages; no modification required here
+# pooling.function.R file must be in same location as working directory
+source(paste0(getwd(),"/pooling.functions.R"))
+
+################################################################
+##### THIS SECTION CREATS IMPLIST REQUIRED FOR ALL ANALYSES
+################################################################
+# make implist.mids from stacked data; no modification required here
+# user can also create implist.mids using a mids object from mice
+implist.mids <- make.mids(data = data,
+                      var.names = var.names,
+                      imp.id = imp.id,
+                      participant.id = participant.id,
+                      missing = missing
+)
+
+
+################################################################
+##### THIS SECTION POOLS DESCRIPTIVE STATISTICS
+################################################################
+# pool descriptives; no modification required here
+descriptives <- pool.descriptives(implist = implist.mids,
+                                  variables = variables)
+
+#print pooled descriptive statistics
+descriptives
+
+################################################################
+##### THIS SECTION POOLS CORRELATIONS
+################################################################
+# pool correlations requires these packages
+library(miceadds)
+library(metaSEM)
+  # use functions to pool correlation, no modification required here
+  cor <- micombine.cor(mi.res=implist.mids, variables=variables)
+  r_matrix   <- attr(cor, "r_matrix")
+  fisher_r <- vec2symMat(cor$fisher_r[1:(length(cor$r)/2)], diag = FALSE, byrow = FALSE) ; rownames(fisher_r) <- colnames(fisher_r) <- variables
+  
+  # print un-transformed correlation matrix  
+  r_matrix
+  
+  # print fisher's transformed correlation matrix
+  fisher_r
+  
+################################################################
+##### THIS SECTION CENTERS VARIABLES FOR REGRESSION ANALYSES
+################################################################
+# if analyses do not require centering, this section isn't necessary and can be skipped
+
+# creating centered variables for regression/moderation analysis
+  # this converts the mids.object back to long format so centering can be done
+  # no modification required here
+  stacked.dat <- complete(implist.mids, action='long', include=TRUE)
+  
+  # specify focal predictor (x) here; must be in quotations
+  x <- "read1"
+  
+  # specify moderator (w) here; must be in quotations
+  w <- "lrnprob1"
+  
+  # this generates centered variables
+    # centering focal predictor and moderator at the mean
+    # this creates new variables that is centered at the mean with "_mu" appended at the end of the variable name
+    # no modification required here
+    stacked.dat[paste0(x,"_mu")]     <- stacked.dat[x] - descriptives$un.transform[x,"mean"]
+    stacked.dat[paste0(w,"_mu")]     <- stacked.dat[w] - descriptives$un.transform[w,"mean"]
+  
+    # centering moderator at 1 SD above/below the mean
+    # this creates new variables that is centered at the mean with "_hi" and "_low" appended at the end of the variable name
+    # no modification required here
+    stacked.dat[paste0(w,"_hi")]  <- stacked.dat[w] - ( descriptives$un.transform[w,"mean"] + descriptives$sd.transform[w,"Inv.-Transformed"] )
+    stacked.dat[paste0(w,"_low")] <- stacked.dat[w] - ( descriptives$un.transform[w,"mean"] - descriptives$sd.transform[w,"Inv.-Transformed"] )
+    
+    
+  # converting long format back to mids object to do regression/moderation analysis
+  # no modification required here
+  implist.mids <- as.mids(stacked.dat)
+  
+################################################################
+##### THIS SECTION POOLS REGRESSION RESULTS
+################################################################
+    
+# defining three moderation analyses with different centering methods
+  # centering focal predictor and moderator at the mean
+  # use variables created from centering above
+  
+  # moderation analysis where focal predictor and moderator are centered at the mean
+  # modify equation as needed
+  lm1 <- 'read2 ~ read1_mu + lrnprob1_mu + read1_mu*lrnprob1_mu'
+  
+  # moderation analysis where focal predictor and moderator are centered at 1 SD above the mean
+  # modify equation as needed
+  lm2 <- 'read2 ~ read1_mu + lrnprob1_hi + read1_mu*lrnprob1_hi'
+  
+  # moderation analysis where focal predictor and moderator are centered at 1 SD below the mean
+  # modify equation as needed
+  lm3 <- 'read2 ~ read1_mu + lrnprob1_low + read1_mu*lrnprob1_low'
+  
+    
+# use function to pool regression results, no modification required here
+  reg1 <- pool.regression(implist.mids,lm1)
+  reg2 <- pool.regression(implist.mids,lm2)
+  reg3 <- pool.regression(implist.mids,lm3)
+  
+  # print regression results
+  reg1
+  reg2
+  reg3 
+  
+  
+  
---
+++ pooling.functions.R
@@ -0,0 +1,264 @@
+# FUNCTIONS FOR POOLING REGRESSION RESULTS # 
+
+#required packages
+library(dplyr)
+library(mice)
+library(psych)
+library(rlist)  
+library(miceadds)
+
+
+
+# make implist from stacked data
+        make.mids <- function(data, var.names, imp.id, participant.id, missing){
+ 
+          # reading in different types of files
+          if ( grepl("csv", data, fixed = TRUE)) {
+            imps <- read.csv(data)
+          } else if ( grepl("dat", data, fixed = TRUE)) {
+            imps <- read.table(data)
+            print(paste("Reading in", data, "file"))
+          } else if ( grepl(".Rdata", data, fixed = TRUE)) {
+            imps <- source(data)
+            print(paste("Reading in", data, "file"))
+          } else if ( grepl("txt", data, fixed = TRUE)) {
+            imps <- read.delim(data)
+            print(paste("Reading in", data, "file"))
+            print( ".txt files must be tab delimited files where the character for decimal points is a period ")
+          } else {
+            writeLines("This function only accepts .csv ,  .dat , .Rdata , and .txt files.
+        \nAll files must have imputations stacked with the original dataset at the beginning
+        \n.txt files must be tab delimited files where the character for decimal points is a period ")
+          }
+
+        imps <- na_if(imps,missing)
+        names(imps) <- var.names
+
+        implist <- as.mids(imps, .imp = imp.id, .id = participant.id)
+        return(implist)
+    }
+
+
+
+#pool descriptives from implist
+    pool.descriptives <- function(implist,variables){
+
+      descriptives <- with(implist, 
+                           expr= describe(list.cbind((mget(variables))))
+                          )
+  
+      pooled.desc <- withPool_MI(descriptives) # pooling untranformed
+      varnames <- rownames(withPool_MI(descriptives))
+      
+      descriptives.analyses <- descriptives$analyses # (for sd and var pooling later)
+      
+      #sd descriptives transformations
+      transformations <- sd_transform(descriptives.analyses)
+      colnames(transformations) <- c("Untransformed", "Log-Transformed", "Sq.Rt-Transformed","Cube-Rt Transformed", "Inv.-Transformed")
+      rownames(transformations) <- variables
+      
+      #variance descriptive transformations
+      transformations2 <- var_transform(descriptives.analyses)
+      colnames(transformations2) <- colnames(transformations)
+      rownames(transformations2) <- variables
+      
+      all.describe <- list(un.transform  = pooled.desc,
+                           sd.transform  = transformations,
+                           var.transform = transformations2)
+
+      
+      return(all.describe)
+      
+}
+
+# pool regression estimates from implist
+    pool.regression <- function(implist, formula){
+
+      # regression analysis
+      fit <- with(implist,{
+        lm(as.formula(formula))
+      })
+    
+      # extract list of analysis results
+      fit.analyses <- fit$analyses
+    
+      
+      # pool coefficients
+      coef <- summary(pool(fit.analyses))
+      
+      # pool regression analysis results for r, r2, sd, var
+      impresults <- list()
+      for (i in 1:length(fit.analyses)) {
+        impresults[[i]] <- as.data.frame(cbind(r  = sqrt(summary(fit$analyses[[i]])$r.squared), # multiple correlation
+                                               r2 = summary(fit$analyses[[i]])$r.squared, # multiple r2
+                                               sd = summary(fit$analyses[[i]])$sigma, # residual std err
+                                               var = (summary(fit$analyses[[i]])$sigma)^2 # residual var
+        ))
+      }
+  
+  # labeling transformatino results
+      r <- r_transform(impresults)
+        colnames(r) <- c("Untransformed", "Fisher's R-Z Transformation")
+        rownames(r) <- c("Multiple Correlation Transformations")
+        r <- t(r)
+        
+      r2 <- r2_transform(impresults)
+        colnames(r2) <- c("Untransformed", "Fisher's R-Z Transformation", "Log-Transformed", "Sq.Rt-Transformed","Cube-Rt Transformed", "Inv.-Transformed")
+        rownames(r2) <- c("Multiple R-Squared Transformations")
+        r2 <- t(r2)
+        
+      sd <- sd_transform(impresults)
+        colnames(sd) <- c("Untransformed", "Log-Transformed", "Sq.Rt-Transformed","Cube-Rt Transformed", "Inv.-Transformed")
+        rownames(sd) <- c("Residual Standard Error Transformations")
+        sd <- t(sd)
+        
+      var <- var_transform(impresults)
+        colnames(var) <- c("Untransformed", "Log-Transformed", "Sq.Rt-Transformed","Cube-Rt Transformed", "Inv.-Transformed")
+        rownames(var) <- c("Residual Variance Transformations")
+        var <- t(var)
+      
+      all.regression <- list(coefficients      = coef,
+                             multiple.r        = r,
+                             multiple.r2       = r2,
+                             residual.std.err  = sd,
+                             residual.variance = var
+                             )
+
+}
+
+# r transform (for multiple correlation)
+    r_transform <- function(data){
+  
+  rfunc <- function(data){
+    # data <- impresults[[1]]
+    R <- data$r
+    fisher <- ((1/2)*(log((1+R)/(1-R))))
+    rinfo <- cbind(R,fisher)
+    #return(R[2,1])
+    return(rinfo)
+  }
+  
+  rlist <- t(lapply(data, rfunc)) # lapply() instead of sapply()
+  
+  r_avg <- function(rlist){
+    # r averages and backtransform
+    ravgs <- withPool_MI(rlist)
+    z <- ravgs[,2]
+    ravgs[,2] <- (exp(2*z)-1)/(exp(2*z)+1)
+    # names(ravgs) <- c("r", "rfisher")
+    return(ravgs)
+  }
+  
+  
+  
+  rs <- r_avg(rlist)
+  return(rs)
+  
+}
+
+# r2 transform (for multiple r2)
+    r2_transform <- function(data){
+  
+  r2func <- function(data){
+    # data <- impresults[[1]]
+    r2 <- data$r2
+    R <- sqrt(r2)
+    fisherr2 <- ((1/2)*(log((1+R)/(1-R))))
+    logr2 <- log(r2)
+    sqrtr2 <- sqrt(r2)
+    cuber2 <- r2^(1/3)
+    invr2 <- 1/r2
+    r2info <- cbind(r2, fisherr2,logr2, sqrtr2, cuber2, invr2)
+    #return(R[2,1])
+    return(r2info)
+  }
+  
+  r2list <- t(lapply(data, r2func)) # lapply() instead of sapply()
+  
+  r2_avg <- function(r2list){
+    # sd averages and backtransform
+    r2avgs <- withPool_MI(r2list)
+    zz <- r2avgs[,2]
+    r2avgs[,2] <- ((exp(2*zz)-1)/(exp(2*zz)+1))^2
+    r2avgs[,3] <- exp(r2avgs[,3])
+    r2avgs[,4] <- r2avgs[,4]^2
+    r2avgs[,5] <- r2avgs[,5]^3
+    r2avgs[,6] <- 1/r2avgs[,6]
+    # names(r2avgs) <- c("r2", "fisherr2",'logr2', "sqrtr2", "cuber2", "invr2")
+    return(r2avgs)
+  }
+  
+  r2s <- r2_avg(r2list)
+  return(r2s)
+  
+}
+
+# sd transform (for descriptive sd OR residual std. err)
+    sd_transform <- function(data){
+  
+  sdfunc <- function(data){
+    # data <- descriptives.analyses[[1]]
+    sd <- data$sd
+    logsd <- log(sd)
+    sqrtsd <- sqrt(sd)
+    cubesd <- sd^(1/3)
+    invsd <- 1/sd
+    sdinfo <- cbind(sd,logsd,sqrtsd,cubesd,invsd)
+    return(sdinfo)
+  }
+  
+  sdlist <- t(lapply(data, sdfunc)) # lapply() instead of sapply()
+  
+  sd_avg <- function(sdlist){
+    # sd averages and backtransform
+    sdavgs <- withPool_MI(sdlist)
+    sdavgs[,2] <- exp(sdavgs[,2])
+    sdavgs[,3] <- sdavgs[,3]^2
+    sdavgs[,4] <- sdavgs[,4]^3
+    sdavgs[,5] <- 1/sdavgs[,5] 
+    # names(sdavgs) <- c("sd", "logsd", "sqrtsd", "cubesd", "invsd")
+    # sdavgs <- sdavgs[1:length(sdavgs),]
+    return(sdavgs)
+  }
+  
+  sds <- sd_avg(sdlist)
+  return(sds)
+  
+}
+
+# var transform (for descriptive var OR residual var)
+    var_transform <- function(data){
+  
+  varfunc <- function(data){
+    # data <- impresults[[1]]
+    var <- (data$sd)^2
+    logv <- log(var)
+    sqrtv <- sqrt(var)
+    cubev <- var^(1/3)
+    invv <- 1/var
+    vinfo <- cbind(var,logv,sqrtv,cubev,invv)
+    return(vinfo)
+  }
+  
+  
+  # vlist1 <- t(lapply(descriptives.analyses, varfunc))
+  vlist <- t(lapply(data, varfunc)) # lapply() instead of sapply()
+  
+  varavg <- function(vlist){
+    # var averages and backtransform
+    varavgs <- withPool_MI(vlist)
+    varavgs[,2] <- exp(varavgs[,2])
+    varavgs[,3] <- varavgs[,3]^2
+    varavgs[,4] <- varavgs[,4]^3
+    varavgs[,5] <- 1/varavgs[,5] 
+    # names(sdavgs) <- c("sd", "logsd", "sqrtsd", "cubesd", "invsd")
+    # sdavgs <- sdavgs[1:length(sdavgs),]
+    return(varavgs)
+    
+    
+  }
+  
+  vars <- varavg(vlist)
+  return(vars)
+  
+}
